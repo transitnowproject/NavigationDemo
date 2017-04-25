4330ebd04d8fdbc501433073a61ea7e3f3ec2fa1
Lovish Verma <vermalovish1@gmail.com>
2017-04-25 10:18:22 +0530

    First

--- app/src/main/java/com/example/lovishverma/fragments/ImportFragment.java
+++ app/src/main/java/com/example/lovishverma/fragments/ImportFragment.java
@@ -18,7 +18,7 @@ public class ImportFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_import,container,true);
+        View rootView = inflater.inflate(R.layout.fragment_import,container,false);
 
 
         return rootView;
